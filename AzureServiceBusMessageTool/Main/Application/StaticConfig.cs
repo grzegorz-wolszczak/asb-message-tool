f833306ae78307995cb4dc1703bea5475de6dbbe
Grzegorz Wolszczak <grzechu81@gmail.com>
2022-03-11 00:31:11 +0100

    hide OnReceiveMessage action choice if Receiver is listening to dead letter queue

--- AzureServiceBusMessageTool/Main/Application/StaticConfig.cs
+++ AzureServiceBusMessageTool/Main/Application/StaticConfig.cs
@@ -13,6 +13,6 @@ public static class StaticConfig
 
    public static readonly TimeSpan NextMessageReceiveDelayTimeSpan = TimeSpan.FromMilliseconds(500);
    public static readonly TimeSpan MessageReceiverReceiveTimeout = TimeSpan.FromSeconds(30);
-   public const string VersionString = "0.9.0";
+   public const string VersionString = "0.10.0";
    public static readonly Version Version = Version.Parse(VersionString);
 }
